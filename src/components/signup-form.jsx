93504fd8eb1e2660afaf03ac0b57aab18a4f44c9
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-02-14 11:22:35 +0530

    Added Signup Page and fixed bg issues

--- src/components/signup-form.jsx
+++ src/components/signup-form.jsx
@@ -3,15 +3,11 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-// import { useRouter } from "next/navigation";
-import Link from "next/link"
 
-
-export function LoginForm({
+export function SgForm({
   className,
   ...props
 }) {
-  // const  router = useRouter()
   return (
     (<div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden ">
@@ -19,9 +15,9 @@ export function LoginForm({
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <h1 className="text-2xl font-bold">Welcome </h1>
                 <p className="text-balance text-muted-foreground">
-                  Login to your account
+                  Create your account
                 </p>
               </div>
               <div className="grid gap-2">
@@ -31,14 +27,17 @@ export function LoginForm({
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
-                    Forgot your password?
-                  </a>
                 </div>
                 <Input id="password" type="password" placeholder = "Enter Password" required />
               </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Confirm Password</Label>
+                </div>
+                <Input id="password" type="password" placeholder = "Confirm Password" required />
+              </div>
               <Button type="submit" className="w-full">
-                Login
+                Register
               </Button>
               <div
                 className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -57,16 +56,16 @@ export function LoginForm({
                 </Button>
               </div>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="/signup" className="underline underline-offset-4">
-                  Register
+                Have an account?{" "}
+                <a href="/login" className="underline underline-offset-4">
+                  Login
                 </a>
               </div>
             </div>
           </form>
           <div className="bg-muted flex">
             <img
-              src="Login.svg"
+              src="signup.svg"
               alt="Image"
               className="" />
           </div>
