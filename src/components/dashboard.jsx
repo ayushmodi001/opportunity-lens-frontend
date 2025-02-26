948040bb821d118a763956c8bcee033ac0d87884
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-02-26 12:00:42 +0530

    Added Sign in with google

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,13 +1,17 @@
 import { AvatarWithDropdown } from "./ui/avatar-with-dropdown"
-import { ThemeToggle } from "./ui/theme-toggle"
+import { ThemeToggle } from "@/components/ui/theme-toggle"
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
 
+export async function Dashboard(){
+    const session = await auth()
 
-export function Dashboard(){
+    if(!session?.user) redirect("/")
     return(
         <>
             <div className="flex justify-between p-7 border-2 border-solid items-center">
                 <div>
-                    <h1>Welcome back to Dashboard</h1>
+                    <h1>Welcome back, {session?.user?.name}</h1>
                 </div>
                 <div className="flex gap-10">
                     <ThemeToggle/>
--- src/components/ui/avatar-with-dropdown.jsx
+++ src/components/ui/avatar-with-dropdown.jsx
@@ -1,6 +1,6 @@
-"use client"
+// "use client"
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar } from "@/components/ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,14 +10,22 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { User, LogOut } from "lucide-react"
+import { auth } from "@/auth"
+import { doLogout } from "@/app/actions"
+import Image from "next/image"
 
-export function AvatarWithDropdown() {
+export async function AvatarWithDropdown() {
+  const session = await auth()
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Avatar className="cursor-pointer">
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            height={75}
+            width={75}
+          />
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
@@ -29,7 +37,9 @@ export function AvatarWithDropdown() {
         </DropdownMenuItem>
         <DropdownMenuItem>
           <LogOut className="mr-2 h-4 w-4" />
-          <span>Logout</span>
+          <form action={doLogout}>
+            <button type="submit">Logout</button>
+          </form>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
